9344db3bd48ebd856653d4d4294c63fea35d1a4a
Daniel Silva <dnlwillian@gmail.com>
2025-05-29 15:04:50 -0300

    feat(autenticação): criação migration usuários (ref #2)

---
+++ backend/services/auth/FC.Auth.Infrastructure/Migrations/20250529171529_CriacaoTabelaUsuarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FC.Auth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelaUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    nome = table.Column<string>(type: "varchar", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "varchar", maxLength: 150, nullable: false),
+                    senha_hash = table.Column<string>(type: "varchar", maxLength: 256, nullable: false),
+                    ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    perfil = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
