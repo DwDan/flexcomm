028c5bb48df12d2dd18fe525523f7e1027b57fb8
Daniel Silva <dnlwillian@gmail.com>
2025-06-03 15:21:59 -0300

    feat(autenticação): endpoint alteração de usuário com testes de integração (ref #13)

---
+++ backend/services/auth/FC.Auth.WebAPI/Feature/Usuario/AlterarUsuario/AlterarUsuarioEnderecoRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace FC.Auth.WebAPI.Feature.Usuario.AlterarUsuario
+{
+    public class AlterarUsuarioEnderecoRequest
+    {
+        public string Logradouro { get; set; } = string.Empty;
+        public string Numero { get; set; } = string.Empty;
+        public string Bairro { get; set; } = string.Empty;
+        public string Cidade { get; set; } = string.Empty;
+        public string Estado { get; set; } = string.Empty;
+        public string Cep { get; set; } = string.Empty;
+    }
+}
---
+++ backend/services/auth/FC.Auth.WebAPI/Feature/Usuario/AlterarUsuario/AlterarUsuarioNumeroTelefoneRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace FC.Auth.WebAPI.Feature.Usuario.AlterarUsuario
+{
+    public class AlterarUsuarioNumeroTelefoneRequest
+    {
+        public string Ddd { get; set; } = string.Empty;
+        public string Numero { get; set; } = string.Empty;
+    }
+}
