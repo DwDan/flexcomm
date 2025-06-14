46c3923dfac524971f6a50ccfd5ddbdc0a771752
Daniel Silva <dnlwillian@gmail.com>
2025-06-14 14:17:58 -0300

    feat(building blocks): fixture para testes com integração kafka (ref #20)

---
+++ backend/buildingblocks/FC.BuildingBlocks.Integration.Tests/Handler/TestEmailHandler.cs
@@ -0,0 +1,17 @@
+﻿using FC.BuildingBlocks.Domain.Messaging;
+using FC.BuildingBlocks.Domain.Messaging.Events;
+
+namespace FC.BuildingBlocks.Integration.Tests.Handler
+{
+    public class TestEmailHandler : IIntegrationEventHandler<EnviarEmailEvent>
+    {
+        public bool FoiExecutado { get; private set; }
+
+        public Task HandleAsync(EnviarEmailEvent @event, CancellationToken cancellationToken)
+        {
+            FoiExecutado = true;
+
+            return Task.CompletedTask;
+        }
+    }
+}
